3a50d0016f2202ec23d0fb564e26c010c6cd54f4
thalles-cg <thalles.pity@gmail.com>
2026-03-13 12:36:35 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+})
